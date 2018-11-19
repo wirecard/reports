95dd628349edccd5dc80fd6d493ed73203a5746a
Tatjana Starcenko <tatjana.starcenko@gmail.com>
2018-11-19 15:45:14 +0100

    Update clean script and remove travis notifications

--- .bin/clean.sh
+++ .bin/clean.sh
@@ -4,20 +4,17 @@ set -e # Exit with nonzero exit code if anything fails
 echo "Starting log cleaner"
 
 export REPO_NAME='reports'
-export REPO_ADDRESS="https://github.com/wirecard/${REPO_NAME}.git"
 
-git clone ${REPO_ADDRESS}
 cd ${REPO_NAME}
 
 TODAY=$(date +%Y-%m-%d)
-
 TWO_WEEKS_AGO=$(date -d 'now - 2 week' +%Y-%m-%d)
 
 LAST_REPORT_DATE=''
 for dir1 in paymentSDK-php/*; do
     for dir2 in $dir1/*; do
         LAST_REPORT_DATE=$(basename -- ${dir2})
-        if [ ${LAST_REPORT_DATE} == ${TWO_WEEKS_AGO} ]; then
+        if [ "${LAST_REPORT_DATE}" == "${TWO_WEEKS_AGO}" ]; then
             echo "Removing directory ${LAST_REPORT_DATE}"
             rm -rf $dir2
         fi
