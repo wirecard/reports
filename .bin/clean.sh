5aa954b955bfa512753502d748c5cb6d2b6f76da
Tatjana Starcenko <tatjana.starcenko@gmail.com>
2018-12-07 14:58:34 +0100

    Update clean.sh

--- .bin/clean.sh
+++ .bin/clean.sh
@@ -21,7 +21,8 @@ git status
 git add -A
 echo "After add"
 git status
-git diff-index --quiet HEAD || git commit -m "Clean up old report files. Travis build: ${TRAVIS_BUILD_WEB_URL}"
+git diff-index --quiet HEAD 
+git commit -m "Clean up old report files. Travis build: ${TRAVIS_BUILD_WEB_URL}"
 echo "After commit"
 git status
 
