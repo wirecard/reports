ce7451b0ea48934074cd24d53b0d665cf0cd6922
Tatjana Starcenko <tatjana.starcenko@gmail.com>
2019-01-04 13:48:59 +0100

    Create html file instead of .md for test results

--- .bin/process.py
+++ .bin/process.py
@@ -1,29 +1,60 @@
-# this to be run on each push to the repo
 import os
-import subprocess
 from junitparser import JUnitXml
 from datetime import datetime
+from string import Template
 
 REPORT_FILE_NAME = 'report.xml'
-TEST_RESULT_FILE_NAME = 'RESULTS.md'
-TEST_RESULT_FILE = os.path.join("docs", TEST_RESULT_FILE_NAME)
-
+INDEX_TEMPLATE_FILE = os.path.join('docs', 'index_template.html')
+INDEX_FILE = os.path.join('docs', 'index.html')
+FULL_REPORT_LINK = "http://htmlpreview.github.io/?https:" \
+                   "//raw.githubusercontent.com/wirecard/reports/blob/master/" \
+                   "$project/$gateway/$date/report.html"
 FAILED_TEST = '<span style="color:red">FAIL</span>'
 PASSED_TEST = '<span style="color:green">PASS</span>'
 DATE_FOLDER_FORMAT = '%Y-%m-%d'
-HEADER_LINE = "# Current status of acceptance tests per instance \n"
-TABLE_HEADER = '''| Project | Test name | Status |
-| ---  | --- | --- |\n'''
+HTML_TABLE_HEADER_LINES = '<h2><b>Current status of acceptance tests</b><h2>\n'
+HTML_TABLE_HEADER_CONTENT = '''<table class="blueTable">
+<thead>
+<tr>
+<th>Project</th>
+<th>Test Name</th>
+<th>Status</th>
+<th>Report link</th>
+</tr></thead>'''
+
 PROJECTS = ["paymentSDK-php", "woocommerce-ee"]
-TEST_RESULTS = {}
+REPORT_LINK_DATA = {}
 
 
-def find_latest_result_file(location):
+def add_to_dict_array(input_dict, key,  value):
+    try:
+        results_arr = input_dict[key]
+        if value not in results_arr:
+            results_arr.append(value)
+            input_dict[key] = results_arr
+    except KeyError as e:
+        results_arr = []
+        if value not in results_arr:
+            results_arr.append(value)
+            input_dict[key] = results_arr
+    return input_dict
+
+
+def get_date_from_report_link_data(project_name, gateway):
+    date_dict = filter(lambda gateway_dict: gateway_dict.get(gateway), REPORT_LINK_DATA[project_name])
+    date = date_dict[0][gateway]
+    return date
+
+
+def find_latest_result_file(location, project, gateway):
+    global REPORT_LINK_DATA
     dates = []
     if os.path.isdir(location):
         for sub_dirs in os.listdir(location):
             dates.append(datetime.strptime(sub_dirs, DATE_FOLDER_FORMAT))
         latest = max(d for d in dates)
+        # Also keep the date for later link in the report
+        REPORT_LINK_DATA = add_to_dict_array(REPORT_LINK_DATA, project, {gateway: latest.strftime(DATE_FOLDER_FORMAT)})
         return os.path.join(location, latest.strftime(DATE_FOLDER_FORMAT), REPORT_FILE_NAME)
     else:
         return None
@@ -48,6 +79,7 @@ def process_results_file(gateway, result_file):
 
 def process_results_files():
     test_results = {}
+    os.chdir('..')
     root_dir = os.getcwd()
     for dirs in os.listdir(root_dir):
         if dirs in PROJECTS:
@@ -55,7 +87,7 @@ def process_results_files():
             gateway_results = []
             for sub_dirs in os.listdir(os.path.join(root_dir, dirs)):
                 gateway = sub_dirs
-                result_file = find_latest_result_file(os.path.join(root_dir, dirs, sub_dirs))
+                result_file = find_latest_result_file(os.path.join(root_dir, dirs, sub_dirs), project, gateway)
                 if result_file:
                     gateway_results.append(process_results_file(gateway, result_file))
             test_results[project] = gateway_results
@@ -69,36 +101,41 @@ def reformat_dictionary(dictionary):
     for project, gateway in dictionary.items():
         for gateway_dict in gateway:
             for gateway_name, results in gateway_dict.items():
-                try:
-                    results_arr = new_dict[gateway_name]
-                    if {project: results} not in results_arr:
-                        results_arr.append({project: results})
-                    new_dict[gateway_name] = results_arr
-                except KeyError as e:
-                    results_arr = []
-                    if {project: results} not in results_arr:
-                        results_arr.append({project: results})
-                    new_dict[gateway_name] = results_arr
+                new_dict = add_to_dict_array(new_dict, gateway_name, {project: results})
     return new_dict
 
 
-# update README.md with latest results
 def create_report_file():
     results = process_results_files()
-    with open(TEST_RESULT_FILE, "w") as result_file:
-        result_file.writelines(HEADER_LINE)
-        for gateway, project_results in results.items():
-            result_file.writelines("## {} \n".format(gateway))
-            result_file.writelines(TABLE_HEADER)
-            for project in project_results:
-                for project_name, test_results in project.items():
-                    for test_name, test_result in test_results.items():
-                        result_file.writelines("| {}  | {} | {} |\n".format(project_name, test_name, test_result))
+    # create contents to put into html template
+    html_table = HTML_TABLE_HEADER_LINES
+    for gateway, project_results in results.items():
+        html_table += "<h3>{}<h3>\n".format(gateway)
+        html_table += HTML_TABLE_HEADER_CONTENT
+        for project in project_results:
+            for project_name, test_results in project.items():
+                for test_name, test_result in test_results.items():
+                    date = get_date_from_report_link_data(project_name, gateway)
+                    report_link = Template(FULL_REPORT_LINK).substitute({"project": project_name,
+                                                                         "gateway": gateway,
+                                                                         "date": date})
+                    html_table += '<tr>'
+                    for value in [project_name, test_name, test_result,
+                                  '<a href="{}">Full report link</a></td>'.format(report_link)]:
+                        html_table += '<td>{}</td>'.format(value)
+                    html_table += '</tr>'
+        html_table += '</table>'
+    # put data into html template
+    with open(INDEX_TEMPLATE_FILE, 'r') as template_file:
+        src = Template(template_file.read())
+        new_text = src.substitute({"table": html_table})
+    with open(INDEX_FILE, 'w') as template_file:
+        template_file.write(new_text)
 
 
 def main():
     create_report_file()
 
-	
+
 if __name__ == "__main__":
     main()
--- .bin/update_report.sh
+++ .bin/update_report.sh
@@ -6,7 +6,7 @@ git config --global user.email "wirecard@travis-ci.org"
 echo "Updating README badge..."
 python .bin/process.py
 
-git add docs/RESULTS.md
-git commit -m "[skip ci] Update docs/RESULTS.md file"
+git add docs/index.html
+git commit -m "[skip ci] Update docs/index.html file with latest test results"
 git push https://$GITHUB_TOKEN@github.com/$TRAVIS_REPO_SLUG HEAD:master
 echo "Successfully updated RESULTS file"
