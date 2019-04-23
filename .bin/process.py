22733dead1eb823572d1ee4b9b271220fa3630f6
Tatjana Starcenko <tatjana.starcenko@gmail.com>
2019-04-23 15:50:32 +0200

    Add new shop systems to reporting

--- .bin/process.py
+++ .bin/process.py
@@ -22,7 +22,7 @@ HTML_TABLE_HEADER_CONTENT = '''<table class="blueTable">
 <th>Report link</th>
 </tr></thead>'''
 
-PROJECTS = ["paymentSDK-php", "woocommerce-ee", "prestashop-ee"]
+PROJECTS = ["paymentSDK-php", "woocommerce-ee", "prestashop-ee", "magento2-ee", "opencart-ee"]
 REPORT_LINK_DATA = {}
 
 
